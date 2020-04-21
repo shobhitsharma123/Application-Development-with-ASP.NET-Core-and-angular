0e9b57ff2e3039f734728c79d7998ebf4b69af00
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-04-21 18:27:07 +0530

    9th commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Application_Development_with_ASP.NET_Core_and_angular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
